6de7250ef816bcdb6c0622133a027b9093c66d21
Ausaf-Pro <ausafhussain010@gmail.com>
2021-11-03 08:08:58 +0530

    User roles/Delete User Using Admin

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,24 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  // Mongooese duplicate errorhandler
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //Wrong JWT Error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json Web Token is invalid, Try Again Later!!`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //JWT Expire
+  if (err.name === "TokenExpireError") {
+    const message = `Json Web Token is Exripred, Try Again Later!!`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
