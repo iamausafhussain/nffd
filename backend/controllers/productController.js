0657328c1bfc4cf916f7db56db65cb69f442527a
Ausaf-Pro <ausafhussain010@gmail.com>
2022-06-04 15:52:34 +0530

    loader added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,15 +17,13 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 //Get all Products
 
-exports.getAllProducts = catchAsyncErrors(async (req, res) => {
+exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
   const resultPerPage = 8;
   const productCount = await Product.countDocuments();
-
   const apiFeature = new ApiFeatures(Product.find(), req.query)
     .search()
     .filter()
     .pagination(resultPerPage);
-
   const products = await apiFeature.query;
   res.status(200).json({
     success: true,
--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -9,41 +9,46 @@ import Loader from "../layout/Loader/loader";
 import { useAlert } from "react-alert";
 
 const Home = () => {
+  const alert = useAlert();
   const dispatch = useDispatch();
   const { loading, error, products, productsCount } = useSelector(
     (state) => state.products
   );
 
   useEffect(() => {
-    // if (error) {
-    //   alert.error(error);
-    //   dispatch(clearErrors());
-    // }
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
     dispatch(getProduct());
-  }, [dispatch]);
+  }, [dispatch, error]);
 
   return (
     <>
-      <>
-        <MetaData title="iShopify" />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <MetaData title="iShopify" />
 
-        <div className="banner">
-          <p>Welcome to iShopify</p>
-          <h1>FIND AMAZING PRODUCTS HERE</h1>
+          <div className="banner">
+            <p>Welcome to iShopify</p>
+            <h1>FIND AMAZING PRODUCTS HERE</h1>
 
-          <a href="#container">
-            <button>
-              <MouseIcon />
-            </button>
-          </a>
-        </div>
-        <h2 className="homeHeading">Featured Products</h2>
+            <a href="#container">
+              <button>
+                <MouseIcon />
+              </button>
+            </a>
+          </div>
+          <h2 className="homeHeading">Featured Products</h2>
 
-        <div className="container" id="container">
-          {products &&
-            products.map((product) => <ProductCard product={product} />)}
-        </div>
-      </>
+          <div className="container" id="container">
+            {products &&
+              products.map((product) => <ProductCard product={product} />)}
+          </div>
+        </>
+      )}
     </>
   );
 };
