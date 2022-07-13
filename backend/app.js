79809b661a789a2eea483e7892b3b3cb5787f759
Ausaf-Pro <ausafhussain010@gmail.com>
2022-07-13 16:51:09 +0530

    added production build for heroki deployment

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,12 @@ const errorMiddleware = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-const dotenv = require("dotenv");
+const path = require("path");
 
 // configuration
-dotenv.config({ path: "backend/config/config.env" });
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,6 +26,12 @@ app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
+
+// deployment
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 //middleware for error
 app.use(errorMiddleware);
