a35a2e185e6021f4d09c782ff39f3d09080d1d05
iamausafhussain <drrespect2000@gmail.com>
2022-07-14 22:03:02 +0530

    added backend configuration

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
 
-// Config
+// Configuration
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
