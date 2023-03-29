0696491c2c976994b9e40c7e15f924e78d3943e3
iamausafhussain <ausafhussain010@gmail.com>
2023-03-29 17:59:57 +0530

    added search fields

--- frontend/src/component/Home/Banner.js
+++ frontend/src/component/Home/Banner.js
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
 import Slider from "react-slick";
+import { Link } from "react-router-dom";
 import "./Home.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ImageOne from "../../images/header.png";
-import ImageTwo from "../../images/banner.jpg";
+import ImageTwo from "../../images/fooddeltwo.jpg";
+import ImageThree from "../../images/fooddelone.jpg";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+import TwoWheelerIcon from "@mui/icons-material/TwoWheeler";
 
 const Banner = () => {
-	const [imageArr, setImageArr] = useState([ImageOne, ImageTwo]);
+	const [imageArr, setImageArr] = useState(
+		{
+			image: ImageOne,
+		},
+		{
+			image: ImageTwo,
+		},
+		{
+			image: ImageThree,
+		}
+	);
 
 	const settings = {
 		dots: false,
@@ -30,7 +44,40 @@ const Banner = () => {
 				<div>
 					<img className="slick-image" src={ImageTwo} alt="Slide 2" />
 				</div>
+				<div>
+					<img className="slick-image" src={ImageThree} alt="Slide 2" />
+				</div>
 			</Slider>
+
+			<div className="slider-description">
+				<div className="slider-description-header slider-info">
+					<p>Are you starving?</p>
+				</div>
+				<div className="slider-description-details slider-info">
+					<p>Withing a few clicks, find the meals that are accesible to you</p>
+				</div>
+				<div className="slider-description-card slider-info">
+					<div className="slider-card-header">
+						<Link to="/products">
+							<button class="slider-delivery-button first-button">
+								<FastfoodIcon />
+								<p>Foods</p>
+							</button>
+						</Link>
+						<Link to="/">
+							<button class="slider-delivery-button">
+								<TwoWheelerIcon />
+								<p>Pickup</p>
+							</button>
+						</Link>
+					</div>
+					<hr />
+					<div className="slider-card-search">
+						<input placeholder="Search Foods " className="slider-input" />
+						<button className="search-button">Find Food</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
